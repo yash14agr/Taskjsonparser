25547b68349c268c82b394e22c97f475c74426b6
Yash <yash1402agr@gmail.com>
2023-12-06 15:07:59 +0530

    Parsed Data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import data from './data/data.json';
+
 
 function App() {
+  const [mainData, setMainData] = useState();
+
+  //Parsing the Data
+  const getData = () => {
+    let mainData1 = JSON.parse(JSON.stringify(data))
+    setMainData(mainData1)
+  }
+
+  //parsing data on initial render
+  useEffect(() => {
+    getData()
+
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p className="Heading">
+        JSON Parser
+      </p>
+
+      {/* Continents Data */}
+      <div className="CardContainer">
+        <p className="CardHead"> Continents :</p>
+        {
+          mainData && mainData.result.continents.map((data, index) => {
+            return (<div key={index} className="card">{data.key}</div>)
+          })
+        }
+      </div>
+
+      {/* Departure Port Data */}
+      <div className="CardContainer">
+        <p className="CardHead"> departure_ports :</p>
+        {
+          mainData && mainData.result.departure_ports.map((data, index) => {
+            return (<div key={index} className="card">{data.key}</div>)
+          })
+        }
+      </div>
+
+      {/* vessels Data */}
+      <div className="CardContainer">
+        <p className="CardHead"> vessels :</p>
+        {
+          mainData && mainData.result.vessels.map((data, index) => {
+            return (<div key={index} className="card">{data.key}</div>)
+          })
+        }
+      </div>
+
+      {/* Sailing Month Data */}
+      <div className="CardContainer">
+        <p className="CardHead"> sailing_months :</p>
+        {
+          mainData && mainData.result.sailing_months.map((data, index) => {
+            return (<div key={index} className="card">{data.key_as_string}</div>)
+          })
+        }
+      </div>
+
+      {/* Total Packages Data */}
+      <div className="CardContainer">
+        <p className="CardHead">Total Packages : </p>
+        <div className="card">
+          {
+            mainData && mainData.result.allPackages.length
+          }
+        </div>
+      </div>
+
+
+      {/* Sailing Dates Data */}
+      <div className="CardContainer">
+        <p className="CardHead"> sailing_dates :</p>
+        <div className="CardContainer">
+
+          {
+
+            mainData && mainData.result.sailing_dates.map((data, index) => {
+              return (<div key={index} className="card">{data.key_as_string}</div>)
+            })
+          }
+        </div>
+      </div>
+
+
+    </>
   );
 }
 
